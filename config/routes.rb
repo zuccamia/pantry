905e46787a4a51b54ce59cf9397f3f50c9ea0009
Ngoc Hoang <zuccamia@gmail.com>
2021-02-14 00:00:25 +0900

    set routes for items, item_amounts, recipes and recipe_amounts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/component', to: 'pages#component', as: 'component'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :item_amounts, only: [:index, :edit, :update, :destroy]
+
+  resources :items, only: [:new, :create]
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_amounts, only: [:new, :create]
+  end
+
+  resources :recipe_amounts, only: [:destroy]
 end
