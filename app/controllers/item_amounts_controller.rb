2e4a2f7b1ce0415e2aa7022f3599a0ae55eef296
Marisa Cassidy <marisancassidy@gmail.com>
2021-02-13 16:30:13 +0900

    Added item-amounts-update

--- app/controllers/item_amounts_controller.rb
+++ app/controllers/item_amounts_controller.rb
@@ -6,4 +6,16 @@ class ItemAmountsController < ApplicationController
   def edit
     @item_amount = ItemAmount.find(params[:id])
   end
+
+  def update
+    @item_amount = ItemAmount.find(params[:id])
+    @item_amount.update(item_amount_params)
+    redirect_to item_amount_path(@item_amount)
+  end
+
+  private
+
+  def item_amount_params
+    params.require(:item_amount).permit(:description, :expiry_date)
+  end
 end
