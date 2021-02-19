8cd94ecde30890321bd292cd7d6f006746711892
Hongjoo Yang <attinore@gmail.com>
2021-02-19 21:10:03 +0900

    updated category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,14 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @item_amounts = Item_amount.new
+  end
+
+
+  def category_params
+    params.require(:category).permit(:main_category, :sub_category)
+  end
 end
