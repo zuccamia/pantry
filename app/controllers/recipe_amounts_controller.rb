a8e4f5420ac43581a7b165f12a4da190c791ed90
Hongjoo Yang <attinore@gmail.com>
2021-02-25 23:46:08 +0900

    recipe amounts controller added

---
+++ app/controllers/recipe_amounts_controller.rb
@@ -0,0 +1,38 @@
+class RecipeAmountsController < ApplicationController
+  def index
+    @recipe_amounts = RecipeAmount.all
+  end
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_amount = RecipeAmount.new
+  end
+
+  def create
+    @recipe_amount = RecipeAmount.new(recipe_amount_params)
+    @recipe_amount.save
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+    @recipe_amount = RecipeAmount.find(params[:id])
+  end
+
+  def update
+    @recipe_amount = RecipeAmount.find(params[:id])
+    @recipe_amount.update(recipe_amount_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    @recipe_amount = RecipeAmount.find(params[:id])
+    @recipe_amount.destroy
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_amount_params
+    params.require(:recipe_amount).permit(:description, :item_id, :recipe_id)
+  end
+end
