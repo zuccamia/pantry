41466f928439f3fc6f7e42c426b4901c6ac780e7
Ngoc Hoang <zuccamia@gmail.com>
2021-03-06 12:31:12 +0900

    remove api module of linebots controller

--- test/controllers/linebots_controller_test.rb
+++ test/controllers/linebots_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Api::LinebotsControllerTest < ActionDispatch::IntegrationTest
+class LinebotsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
