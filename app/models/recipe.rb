f84e60716fcca21c390b097f3c014176d5bea76b
Hongjoo Yang <attinore@gmail.com>
2021-03-07 22:17:09 +0900

    recipe and ingredients now in the same page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_amounts, dependent: :destroy
   has_many :items, through: :recipe_amounts
-  accepts_nested_attributes_for :recipe_amounts, update_only: true
-
+  accepts_nested_attributes_for :recipe_amounts, allow_destroy: true, update_only: true, reject_if: :all_blank
   validates :recipe_name, presence: true, length: { minimum: 5 }
   acts_as_taggable_on :tags
 
--- app/models/recipe_amount.rb
+++ app/models/recipe_amount.rb
@@ -1,6 +1,6 @@
 class RecipeAmount < ApplicationRecord
   belongs_to :item
-  belongs_to :recipe
+  belongs_to :recipe, optional: true
   validates_presence_of :recipe
 
   validates :description, presence: true, length: { minimum: 2 }
